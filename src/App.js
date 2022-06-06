2a32287b31f6002adf91526f2ea67271d1136b44
stetsonharding <stetson2001@msn.com>
2022-06-06 01:05:31 -0700

    google fonts and global styles.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+body{
+  font-family: 'Open Sans', sans-serif;
+ 
+  h1, h2,h3 {
+    font-family: 'Righteous', cursive;
+  }
+}
+`;
+
 function App() {
   return (
-    <div className="App">
-      <p>Pizza Reiah</p>
-    </div>
+    <>
+      <GlobalStyles />
+      <div className="App">
+        <p>Pizza Reiah</p>
+        <h2>Testing Pizza Heading Font</h2>
+      </div>
+    </>
   );
 }
 
